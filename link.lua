ba5400851836fbec1a8025cb91afe8f141d40754
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-20 16:45:57 +0700

    Update link.lua

--- link.lua
+++ link.lua
@@ -3,4 +3,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/PC.lua"))()
 
 --PE
-loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/PC.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/PE.lua"))()

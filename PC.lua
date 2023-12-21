76e7ceb61d0713eab33acfb8c76ac0a2afdaaead
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-21 14:33:24 +0700

    Update PC.lua

--- PC.lua
+++ PC.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B8%9B%E0%B8%81%E0%B8%95%E0%B8%B4%20maru%20hub%20%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%9E
+loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B8%9B%E0%B8%81%E0%B8%95%E0%B8%B4%20maru%20hub%20%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%9E"))()

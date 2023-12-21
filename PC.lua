12e98100d319b16b817563f2c0ad4f0eb1bcf061
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-21 13:37:24 +0700

    Update PC.lua

--- PC.lua
+++ PC.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/ไก่ตัน maru hub.lua"))()
+--PE
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/PE.lua"))()
+
+--ไก่ตัน
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/ไก่ตัน maru hub.lua"))()
+
+--ปกติ
+loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/ปกติ maru hub ทุกแมพ"))()

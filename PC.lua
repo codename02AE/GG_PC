a86e114699937555824793f5b9f8bcd9bd4ac413
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-26 12:57:46 +0700

    Update PC.lua

--- PC.lua
+++ PC.lua
@@ -9,7 +9,7 @@ print("Starter")
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/PE.lua"))()
 
 --ไก่ตัน
---loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B9%84%E0%B8%81%E0%B9%88%E0%B8%95%E0%B8%B1%E0%B8%99%20maru%20hub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B9%84%E0%B8%81%E0%B9%88%E0%B8%95%E0%B8%B1%E0%B8%99%20maru%20hub.lua"))()
 
 --ปกติ
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B8%9B%E0%B8%81%E0%B8%95%E0%B8%B4%20maru%20hub%20%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%9E"))()
@@ -17,4 +17,4 @@ print("Starter")
 --ล่าค่าหัวผล
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/ล่าค่าหัวผล.lua"))()
 
-loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+--loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()

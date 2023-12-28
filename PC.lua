d95a6cd4ccdecea7ab9dd6b101bb8481b9926c73
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-28 20:29:26 +0700

    Update PC.lua

--- PC.lua
+++ PC.lua
@@ -22,4 +22,4 @@ print("Starter")
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/Blade_Ball.lua"))()
 
 --Mukuro HUB
---loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()

f5adec3f6961bd9136454ad3d4dd7b8c3f124bed
codename02AE <154414340+codename02AE@users.noreply.github.com>
2023-12-30 19:37:33 +0700

    Update PC.lua

--- PC.lua
+++ PC.lua
@@ -15,7 +15,7 @@ print("Starter")
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B9%84%E0%B8%81%E0%B9%88%E0%B8%95%E0%B8%B1%E0%B8%99%20maru%20hub.lua"))()
 
 --ปกติ
-loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B8%9B%E0%B8%81%E0%B8%95%E0%B8%B4%20maru%20hub%20%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%9E"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/%E0%B8%9B%E0%B8%81%E0%B8%95%E0%B8%B4%20maru%20hub%20%E0%B8%97%E0%B8%B8%E0%B8%81%E0%B9%81%E0%B8%A1%E0%B8%9E"))()
 
 --ล่าค่าหัวผล
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/ล่าค่าหัวผล.lua"))()
@@ -24,6 +24,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/ma
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/codename02AE/GG_PC/main/Blade_Ball.lua"))()
 
 --Mukuro HUB
--- loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
 
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/DonateMe/main/OldBf"))()

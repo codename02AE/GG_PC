6a2c457b1c9e659f9c8f36d0d29aafccb269ff68
codename02AE <154414340+codename02AE@users.noreply.github.com>
2024-01-02 19:07:32 +0700

    Update rejoin.lua

--- rejoin.lua
+++ rejoin.lua
@@ -1,12 +1,12 @@
--- repeat wait() until game.CoreGui:FindFirstChild('RobloxPromptGui')
+repeat wait() until game.CoreGui:FindFirstChild('RobloxPromptGui')
 
--- local lp,po,ts = game:GetService('Players').LocalPlayer,game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService('TeleportService')
+local lp,po,ts = game:GetService('Players').LocalPlayer,game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService('TeleportService')
 
--- po.ChildAdded:connect(function(a)
---     if a.Name == 'ErrorPrompt' then
--- 		repeat
---             ts:Teleport(game.PlaceId)
---             sleep(0.1)
---         until false
---     end
--- end)
+po.ChildAdded:connect(function(a)
+    if a.Name == 'ErrorPrompt' then
+		repeat
+            ts:Teleport(game.PlaceId)
+            sleep(0.1)
+        until false
+    end
+end)
